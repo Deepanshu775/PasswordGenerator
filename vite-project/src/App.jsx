5c85a28858a178f933f30071f230bd61a7276a36
Deepanshul <96393045+Deepanshu775@users.noreply.github.com>
2024-04-26 21:17:09 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,52 +1,86 @@
-import { useState ,useCallback} from 'react'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from 'react';
+import './App.css';
 
 function App() {
+  const [length, setLength] = useState(8);
+  const [number, setNumber] = useState(false);
+  const [character, setCharacter] = useState(true);
+  const [password, setPassword] = useState('');
   
-  const  [length,setlength]=useState(8);
-  const [number,setnumber]=useState(false);
-  const[character,setcharacter]=useState(true);
-  const[password,setpassword]=useState("")
-  const passwordgenerator=useCallback(()=>{
-    
-    let pass=""
-    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklnmopqrstuvwxyz"
-    if(number) str+="0123456789"
-    if(character) str+="!~@#$%^&*{}"
-    for(int i=1;i<=Array.length();i++) {
-      let char  =Math.floor(Math.random()*str.length+1)
-      pass=str.charat(char)
+  const passwordref=useRef(null)
+
+  const generatePassword = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (number) str += "0123456789";
+    if (character) str += "!~@#$%^&*{}";
+
+    for (let i = 0; i < length; i++) {
+      let charIndex = Math.floor(Math.random() * str.length);
+      pass += str.charAt(charIndex);
     }
-  },[length,number,character,setpassword])
+
+    setPassword(pass);
+  }, [length, number, character]);
+
+  const CopyPasswordToClipboard = useCallback(() => {
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+  
+
+  useEffect(() => {
+    generatePassword();
+  }, [length, number, character, generatePassword]);
+ 
+
+  
   return (
     <>
-    <div className='HEADING'>Password Generator</div>
+      <div className='HEADING'>Password Generator</div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Number</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+          <input
+            type='checkbox'
+            defaultChecked={number}
+            id='numberinput'
+            onChange={() => { setNumber((prev) => !prev); }}
+          />
+          <label htmlFor='numberinput'>Numbers</label>
+
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="mb-2 navbar-nav me-auto mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <label htmlFor="customRange1" className="form-label">Length:{length}</label>
+                <input type="range" className="form-range" id="customRange1" min={6} max={50} value={length} onChange={(e) => { setLength(parseInt(e.target.value)); }} />
               </li>
+
               <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+                <input
+                  type='checkbox'
+                  defaultChecked={character}
+                  id='characterinput'
+                  onChange={() => { setCharacter((prev) => !prev); }}
+                />
+                <label htmlFor='characterinput'>Character</label>
               </li>
-             
-             
             </ul>
             <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">copy</button>
+              <input
+                className="form-control me-2"
+                type="input"
+                placeholder="Password"
+                aria-label="Search"
+                value={password}
+                readOnly
+                ref={passwordref}
+              />
+              <button onClick={CopyPasswordToClipboard} className="btn btn-outline-success" type="submit">Copy</button>
             </form>
           </div>
         </div>
       </nav>
     </>
   );
-} 
+}
 
 export default App;
